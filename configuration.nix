9c6382b13322863597f0bfa1fd2ed08769439489
Leo Newton <leo.newton@icloud.com>
2025-04-23 21:03:52 -0700

    Update configuration.nix - Added Steamdeck Sysctls

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,27 @@ in
     loader.efi.canTouchEfiVariables = true;
     kernelParams = [ "quiet" "splash" "console=/dev/null" "tpm=false" "tpm_tis.force=0" "8250.nr_uarts=0" ];
     loader.timeout = 0;
-    kernel.sysctl."kernel.split_lock_mitigate"= "0";
+    kernel.sysctl = {
+        # From steamos-customizations-jupiter
+        # https://github.com/Jovian-Experiments/steamos-customizations-jupiter/commit/bc978e8278ca29a884b1fe70cf2ec9e1e4ba05df
+        "kernel.sched_cfs_bandwidth_slice_u" = 3000;
+        "kernel.sched_latency_ns" = 3000000;
+        "kernel.sched_min_granularity_ns" = 300000;
+        "kernel.sched_wakeup_granularity_ns" = 500000;
+        "kernel.sched_migration_cost_ns" = 50000;
+        "kernel.sched_nr_migrate" = 128;
+        "kernel.split_lock_mitigate" = 0;
+
+        # > This is required due to some games being unable to reuse their TCP ports
+        # > if they're killed and restarted quickly - the default timeout is too large.
+        #  - https://github.com/Jovian-Experiments/steamos-customizations-jupiter/commit/4c7b67cc5553ef6c15d2540a08a737019fc3cdf1
+        "net.ipv4.tcp_fin_timeout" = 5;
+        
+        # > USE MAX_INT - MAPCOUNT_ELF_CORE_MARGIN.
+        # > see comment in include/linux/mm.h in the kernel tree.
+        #  - https://github.com/Jovian-Experiments/steamos-customizations-jupiter/commit/e21954bb4743635a9c53016def5158469fa6d7a8
+        "vm.max_map_count" = 2147483642;
+      };
     
     plymouth.enable = true;
     initrd = {
