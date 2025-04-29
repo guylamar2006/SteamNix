ba58638496c3307090b7e2f978f5c751906fdd4a
Leo Newton <leo.newton@icloud.com>
2025-04-28 21:40:14 -0700

    Clean up - Removed custom resolution.

--- configuration.nix
+++ configuration.nix
@@ -96,7 +96,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.gamescope}/bin/gamescope -W 1920 -H 1080 -f -e --xwayland-count 2 --hdr-enabled  --hdr-itm-enabled -- steam -pipewire-dmabuf -gamepadui -steamos > /dev/null 2>&1";
+        command = "${pkgs.gamescope}/bin/gamescope -f -e --xwayland-count 2 --hdr-enabled  --hdr-itm-enabled -- steam -pipewire-dmabuf -gamepadui -steamos > /dev/null 2>&1";
         user = "steamos";
       };
     };
